2cd393128fb0d04f12c9196ec44f3e50efe1e974
surya-lohan <suryanshlohan@gmail.com>
2025-09-18 16:46:58 +0530

    Create CaptainContext

--- Frontend/src/pages/CaptainSignup.jsx
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/Logo.png'
 import { useState } from 'react'
-
+import { CaptainDataContext } from '../context/CaptainContext'
 const CaptainSignup = () => {
   const [email , setEmail] = useState('');
       const [password , setPassword] = useState('');
       const [captainData , setCaptainData] = useState({});
       const [firstName , setFirstName] = useState(''); 
       const [lastName , setLastName] = useState(''); 
+      const[vehicleType , setVehicleType] = useState('');
+      const[vehicleColor, setVehicleColor] = useState('');
+      const[vehiclePlate, setVehiclePlate] = useState('');
+      const[vehicleCapacity, setVehicleCapacity] = useState('');
+      const {captain , setCaptain} = useContext(CaptainDataContext);
+
       const submitHandler = (e) => {
         e.preventDefault();
-        
-        
-        
         setEmail('');
         setFirstName('');
         setLastName('');
@@ -34,12 +37,28 @@ const CaptainSignup = () => {
         <input value={email} onChange={(e) => setEmail(e.target.value)} className='bg-[#eeeeee] mb-7  rounded px-4 py-2 border w-full text-lg placeholder:text-base' type="email" required placeholder='email@example.com' />
         <h3 className='text-lg font-medium'>Enter password</h3>
         <input value={password} onChange={(e) => setPassword(e.target.value)} className='bg-[#eeeeee] mb-7  rounded px-4 py-2 border w-full text-lg placeholder:text-base' type="password" placeholder='password'/>
-        <button className='bg-[#111] mb-7 text-white font-semibold rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'>Login</button>
-        <p className='text-center'>Already have an account? <Link to={'/user/login'} className='text-blue-700'>Login here</Link> </p>
+        
+        <h3 className='text-lg font-medium mb-2'>Vehicle Information</h3>
+        <div className='flex gap-2 mb-7'>
+          <select value={vehicleType} onChange={(e) => setVehicleType(e.target.value)} className='bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-lg' required>
+            <option value="">Select Vehicle Type</option>
+            <option value="car">Car</option>
+            <option value="auto">Auto</option>
+            <option value="moto">Moto</option>
+          </select>
+          <input value={vehicleColor} onChange={(e) => setVehicleColor(e.target.value)} className='bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-lg placeholder:text-base' type="text" required placeholder='Vehicle Color' />
+        </div>
+        <div className='flex gap-2 mb-7'>
+          <input value={vehiclePlate} onChange={(e) => setVehiclePlate(e.target.value)} className='bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-lg placeholder:text-base' type="text" required placeholder='Vehicle Plate' />
+          <input value={vehicleCapacity} onChange={(e) => setVehicleCapacity(e.target.value)} className='bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-lg placeholder:text-base' type="number" required placeholder='Vehicle Capacity' />
+        </div>
+        
+        <button className='bg-[#111] mb-7 text-white font-semibold rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'>Sign Up</button>
+        <p className='text-center'>Already have an account? <Link to={'/captain/login'} className='text-blue-700'>Login here</Link> </p>
       </form>
       </div>
       <div>
-        <Link to={'/captain/login'} className='bg-[#10B461] flex items-center justify-center mb-7 text-white font-semibold rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'>Sign In as Captain</Link>
+        <Link to={'/user/signup'} className='bg-[#d5622d] flex items-center justify-center mb-7 text-white font-semibold rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'>Sign Up as User</Link>
       </div>
     </div>
   )
